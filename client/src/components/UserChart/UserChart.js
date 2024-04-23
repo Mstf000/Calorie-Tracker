ce57d6d9877b3f58cc20c2f481e097bd44882675
Mstf000 <mostafacrash400@gmail.com>
2024-04-23 14:15:24 +0200

    Stable Version

--- client/src/components/UserChart/UserChart.js
+++ client/src/components/UserChart/UserChart.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Pie } from "react-chartjs-2";
 import axios from "axios";
+import { Chart, CategoryScale, LinearScale, Title } from 'chart.js';
+import { BarElement, BarController } from 'chart.js';
 
 const Delayed = ({ children, waitBeforeShow = 4500 }) => {
   const [isShown, setIsShown] = useState(false);
@@ -14,61 +16,58 @@ const Delayed = ({ children, waitBeforeShow = 4500 }) => {
   return isShown ? children : null;
 };
 
-
 const UserChart = () => {
   const [chartData, setChartData] = useState({});
 
-  async function getData() {
-    let username = [];
-    let calories = [];
-    await axios
-      .get("http://localhost:5000/calorie/")
-      .then((res) => {
-        console.log(res);
-        for (const dataObj of res.data) {
+  useEffect(() => {
+    async function getData() {
+      let username = [];
+      let calories = [];
+      try {
+        const response = await axios.get("http://localhost:5000/calorie/");
+        for (const dataObj of response.data) {
           username.push(dataObj.username);
           calories.push(parseInt(dataObj.calories));
-          console.log(username, calories);
         }
         setChartData({
           labels: username,
-          datasets: [
-            {
-              label: "Calories",
-              data: calories,
-              backgroundColor: [
-                "#f42f42",
-                "#5ab950",
-                "#fe812a",
-                "#ffc748",
-                "#6b71c7",
-                "#8661d1",
-                "#8a2cba"
-              ],
-              borderColor: [
-                "#f42f42",
-                "#5ab950",
-                "#fe812a",
-                "#ffc748",
-                "#6b71c7",
-                "#8661d1",
-                "#8a2cba"
-              ],
-              borderWidth: 2,
-            },
-
-          ],
+          datasets: [{
+            label: "Calories",
+            data: calories,
+            backgroundColor: [
+              "#f42f42",
+              "#5ab950",
+              "#fe812a",
+              "#ffc748",
+              "#6b71c7",
+              "#8661d1",
+              "#8a2cba"
+            ],
+            borderColor: [
+              "#f42f42",
+              "#5ab950",
+              "#fe812a",
+              "#ffc748",
+              "#6b71c7",
+              "#8661d1",
+              "#8a2cba"
+            ],
+            borderWidth: 1,
+          }],
         });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    console.log(username, calories);
-  }
-
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    getData();
+  }, []);
 
   useEffect(() => {
-    getData();
+    Chart.register(CategoryScale, LinearScale, Title, BarElement, BarController);
+
+    return () => {
+      Chart.unregister(BarElement, BarController);
+    };
   }, []);
 
   return (
@@ -88,17 +87,18 @@ const UserChart = () => {
           <Pie
             data={chartData}
             options={{
-              title: {
-                text: "Calorie per User",
-                fontSize: 10,
-                fontColor: "#212529",
+              plugins: {
+                title: {
+                  display: true,
+                  text: "Calorie per User",
+                  font: { size: 20 },
+                  color: "#212529",
+                },
               },
               maintainAspectRatio: true,
             }}
           />
         </Delayed>
-
-
       </div>
     </div>
   );
